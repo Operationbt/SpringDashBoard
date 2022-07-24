07b703a77ef36ee6e33d0765541ae9e13b706e74
operationbt <operationbt12@gmail.com>
2022-07-25 04:18:07 +0900

    이클립스의 데이터베이스 커넥션으로 jdbc연결해서 mySQL연동 확인

--- src/main/java/com/spdb/springdashboard/HomeController.java
+++ src/main/java/com/spdb/springdashboard/HomeController.java
@@ -1,5 +1,8 @@
 package com.spdb.springdashboard;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -14,9 +17,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
+
 @Controller
 public class HomeController {
-	
+	static final String DB_URL = "jdbc:mysql://localhost:3306/springdashboarddb?serverTimezone=UTC";
+	static final String USER = "root";
+	static final String PASS = "";
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
@@ -33,6 +39,21 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		model.addAttribute("myName", " 백지훈");
+		
+		//db연동 테스트
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			System.out.println("DB 연결 완료");
+			model.addAttribute("dbConn", "DB 연결 완료");
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println("JDBC 드라이버 로드 에러");
+			model.addAttribute("dbConn", "JDBC 드라이버 로드 에러");
+		} catch (SQLException e) {
+			System.out.println("DB 연결 오류");
+			model.addAttribute("dbConn", "DB 연결 오류");
+		}
 		
 		return "home";
 	}
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,4 +1,6 @@
-
+<!-- web.xml에서 인코딩필터 설정해도 jsp파일 안에 있는 한글은 깨짐-->
+<!-- jsp페이지 인코딩을 utf-8로 설정해야함 -->
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%> 
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ page session="false" %>
 <html>
@@ -12,5 +14,6 @@
 
 <P>  The time on the server is ${serverTime}. </P>
 <P>  My name is ${myName}. </P>
+<P>DB연결 상태 : ${dbConn}</P> 
 </body>
 </html>
